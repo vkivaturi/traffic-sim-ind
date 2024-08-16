df6cbdf518d28f0158ba868d2bd4c325554765d9
vkivaturi <vijay.ivaturi@gmail.com>
2024-08-16 20:18:08 +0530

    Road setup

--- main.js
+++ main.js
@@ -1,20 +1,39 @@
 import createVehicle from "./src/vehicle.js";
 
 let ctx;
-let raf;
 let vehicleArr = [];
 
 function init() {
     ctx = document.getElementById("canvas").getContext("2d");
-
-    for (var i = 0; i < 20; i++) {
-        vehicleArr[i] = createVehicle(ctx, 100 + i*20, 800);
-    }
+    createVehicles();
     window.requestAnimationFrame(updateVehiclePositions);
 }
 
+function createRoad() {
+    ctx.fillStyle = "black"; 
+    ctx.strokeStyle = "white"; 
+    ctx.setLineDash([10, 15]);
+    ctx.fillRect(80, 0, 100, 800);
+    //ctx.clearRect(45, 45, 60, 60);
+    ctx.strokeRect(80, 0, 50, 800);
+}
+
+//Create vehicles required in simulation
+function createVehicles() {
+    let x,y;
+    for (var i = 0; i < 2; i++) {
+        //Create new vehicle at a different horizontal position
+        x = 100 + i*60;
+        y = 800;
+        vehicleArr[i] = createVehicle(ctx, x, y);
+    }
+}
+
+//Update positions of the vehicles on the path
 function updateVehiclePositions() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    createRoad();
+
     for (var index = 0; index < vehicleArr.length; index++) {
         if (vehicleArr[index].y > 0) {
             vehicleArr[index].x += vehicleArr[index].vx;
@@ -25,4 +44,5 @@ function updateVehiclePositions() {
     window.requestAnimationFrame(updateVehiclePositions);
 }
 
+//Start simulation with the init function
 init();
--- src/vehicle.js
+++ src/vehicle.js
@@ -3,14 +3,14 @@ export default function createVehicle(ctx, _x, _y) {
         x: _x,
         y: _y,
         vx: 0,
-        vy: -5,
-        radius: 5,
+        vy: -4,
+        radius: 20,
         color: "blue",
         draw() {
             ctx.beginPath();
             ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
             ctx.closePath();
-            ctx.fillStyle = this.color;
+            ctx.fillStyle = "yellow";
             ctx.fill();
         },
     };
